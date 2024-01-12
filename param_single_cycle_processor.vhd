4e3f77ea84cac0b0921615bdec6db0b8985bfa25
ivan-plakhotnyk <125100182+ivan-plakhotnyk@users.noreply.github.com>
2024-01-12 18:14:59 +0000

    Update param_single_cycle_processor.vhd

--- param_single_cycle_processor.vhd
+++ param_single_cycle_processor.vhd
@@ -7,60 +7,66 @@ use work.DigEng.ALL;
 entity param_single_cycle_processor is
     generic( size     : NATURAL := 50;
              num_regs : NATURAL := 8);
-    Port (  CLK, RST, WEN, SEL : in STD_LOGIC; -- clock, reset, write enable, mux select
-            -- SH controls number of bits for shift
-            SH       : in  UNSIGNED(size-1 downto 0);
-            -- opcode controls ALU operation of fixed size
-            opcode   : in  STD_LOGIC_VECTOR (size-1 downto 0);
-            -- IMM input vector to processor (immediate value)
-            IMM      : in  STD_LOGIC_VECTOR(size-1 downto 0); 
-            -- RREG_A controls mux for register read channel A (thelog2 for addressing)
-            RREG_A   : in  UNSIGNED(log2(size)-1 downto 0); 
-            -- RREG_B controls mux for register read channel B
-            RREG_B   : in  UNSIGNED(size-1 downto 0); 
-            -- WREG controls active register when write enabled (encoded)
-            WREG     : in  UNSIGNED(log2(size)-1 downto 0); 
-            PROC_OUT : out STD_LOGIC_VECTOR(size-1 downto 0); -- system output
-            -- FLAGS display errors generated in process (fixed size)
-            FLAGS    : out STD_LOGIC_VECTOR(7 downto 0)); 
+    Port (  CLK, RST, START: in STD_LOGIC; -- clock, reset, write enable, mux select
+            PROC_OUT : out STD_LOGIC_VECTOR(size-1 downto 0);
+            A: in STD_LOGIC_VECTOR(size-1 downto 0);
+            B: in STD_LOGIC_VECTOR(size-1 downto 0)
+            ); 
 end param_single_cycle_processor;
 
 architecture Behavioral of param_single_cycle_processor is
+
+    
     -- different fsm states are:
     -- full description in state assignment
     -- IDLE : reset state, LOAD_B : R1 <= B(IMM), LOAD_A : R2 <= A(IMM)
     -- SET_BITS : R6 <= R0 + 1, 
     type fsm_states is (IDLE, LOAD_B, LOAD_A, MASK, SET_BITS, TEST_BIT,
-                         ADD_A, SHIFT_B, SHIFT_A, UPDATE, OUTPUT);
+                         ADD_A, SHIFT_B, SHIFT_A, UPDATE, OUTPUT_RESULT);
     signal state, next_state : fsm_states; -- of type current state and next state
-
+    -- IMM input vector to processor (immediate value)
+    signal IMM   :  STD_LOGIC_VECTOR(15 downto 0);
     -- add internal signals here
-    signal START : STD_LOGIC; -- controls FSM transition from IDLE
+    signal SEL : STD_LOGIC;
     signal ALU_OUT : STD_LOGIC_VECTOR(size-1 downto 0); -- controls FSM transitions from TEST_BIT to OUTPUT
     signal mux_out: STD_LOGIC_VECTOR(size-1 downto 0);
     signal reg_bank_out_B : STD_LOGIC_VECTOR(size-1 downto 0);
     signal reg_bank_out_A : STD_LOGIC_VECTOR(size-1 downto 0);
+    -- SH controls number of bits for shift
+    signal SH_int: UNSIGNED(log2(size)-1 downto 0);
+    -- opcode controls ALU operation of fixed size
+    signal opcode_int   : STD_LOGIC_VECTOR (3 downto 0);
+            -- RREG_A controls mux for register read channel A (thelog2 for addressing)
+    signal RREG_A   :   UNSIGNED(log2(num_regs)-1 downto 0); 
+            -- RREG_B controls mux for register read channel B
+    signal RREG_B   :   UNSIGNED(log2(num_regs)-1 downto 0); 
+            -- WREG controls active register when write enabled (encoded)
+    signal WREG     :   UNSIGNED(log2(num_regs)-1 downto 0); 
+            -- FLAGS display errors generated in process (fixed size)
+    signal FLAGS_int    :  STD_LOGIC_VECTOR(7 downto 0);
+    
+    signal WEN:  STD_LOGIC;
 begin
     -- add combinational logic here
     PROC_OUT <= STD_LOGIC_VECTOR(ALU_OUT);
     mux_out <= (IMM) when SEL = '1' else
-               reg_bank_out_B; -- includes 0 and undefined
+               reg_bank_out_A; -- includes 0 and undefined
    
 ALU_ent : entity work.param_alu
-    generic map (size => size)
+    generic map (data_size => size)
     port map(  A => STD_LOGIC_VECTOR(mux_out), -- input A of [data_size]
-               B => STD_LOGIC_VECTOR(reg_bank_out_A), -- input B of [data_size]
-               opcode => opcode, -- log2(13) => 4 bit addressing
-               SH     => SH, -- shift addressing
+               B => STD_LOGIC_VECTOR(reg_bank_out_B), -- input B of [data_size]
+               opcode => opcode_int, -- log2(13) => 4 bit addressing
+               SH     => SH_int, -- shift addressing
                Output => ALU_OUT, -- ALU output [data_size]
-               flags  => FLAGS); -- 1 bit per flag (fixed 8-bit)
+               flags  => FLAGS_int); -- 1 bit per flag (fixed 8-bit)
 
 reg_bank : entity work.param_reg_bank
     generic map ( data_size => size, -- default bus size
                   num_regs => num_regs) -- default number of registers
     port map ( clk  => CLK,
                rst  => RST, 
-               wen  => wen,
+               wen  => WEN,
                wreg => WREG, -- vector points to register being written to
                r_reg_A => RREG_A, -- read reg bank A -- controls mux 1 for data output (read)
                r_reg_B => RREG_B, -- read reg bank B -- controls mux 2 for data output (read)
@@ -108,7 +114,7 @@ begin
             if (UNSIGNED(ALU_OUT) = 1) then
                 next_state <= ADD_A;
             else -- i.e. alu_out = 0
-                next_state <= SHIFT_A;
+                next_state <= SHIFT_B;
             end if;
         -- add multiplier to result : R3 <= R3 + R2
         when ADD_A =>
@@ -123,134 +129,44 @@ begin
             if (UNSIGNED(ALU_OUT) > 0) then -- if theres still remaining bits then
                 next_state <= TEST_BIT; -- repeat process
             else -- i.e. operation complete : OUT <= R3
-                next_state <= OUTPUT; -- output result
+                next_state <= OUTPUT_RESULT; -- output result
             end if;
+        when OUTPUT_RESULT =>
+            next_state <= state;
         end case;
 end process state_transitions;
 -- FSM outputs:
 --     type fsm_states is (IDLE, LOAD_B, LOAD_A, MASK, SET_BITS, TEST_BIT, ADD_A, SHIFT_B, SHIFT_A, UPDATE, OUTPUT);
 -- signal <= '1' when state = some_state or...    
-
+--Output for the statets of the FSM(According to the table of outputs)
+opcode_int <= "0000" when state = IDLE or state = LOAD_B or state = LOAD_A or state = OUTPUT_RESULT else
+              "1000" when state = MASK else
+              "1100" when state = SET_BITS else
+              "0100" when state = TEST_BIT else
+              "1010" when state = ADD_A else
+              "1101" when state = SHIFT_B else 
+              "1100" when state  = SHIFT_A else
+              "1001" when state = UPDATE else
+              (others=>'0');
+SH_int <= to_unsigned(3, log2(size)) when  state =  SET_BITS else
+          to_unsigned(1, log2(size)) when  state = SHIFT_B or state = SHIFT_A else to_unsigned(0, log2(size));
+SEL <= '1' when state = LOAD_A or state = LOAD_B else '0';
+IMM <= A when state = LOAD_A else
+       B when state = LOAD_B else
+       (others=>'0');
+RREG_A <= to_unsigned(6, log2(num_regs)) when state = SET_BITS or state = TEST_BIT else
+          to_unsigned(2, log2(num_regs)) when state = ADD_A or state = SHIFT_A else
+          to_unsigned(1, log2(num_regs)) when state = SHIFT_B else
+          to_unsigned(0, log2(num_regs)) when state = MASK else
+          to_unsigned(7, log2(num_regs)) when state = UPDATE else
+          to_unsigned(3, log2(num_regs)) when state = OUTPUT_RESULT else to_unsigned(0, log2(num_regs));
+RREG_B <= to_unsigned(1, log2(num_regs)) when state = TEST_BIT else
+          to_unsigned(3, log2(num_regs)) when state = ADD_A else to_unsigned(0, log2(num_regs));
+WEN <= '1' when state = LOAD_A or state = LOAD_B or state = MASK or state = SET_BITS or
+        state = ADD_A or state = SHIFT_B or state = SHIFT_A or state = UPDATE else '0';
+WREG <= to_unsigned(1, log2(num_regs)) when state = LOAD_B or state = SHIFT_B else
+        to_unsigned(2, log2(num_regs)) when state = LOAD_A or state = SHIFT_A else
+        to_unsigned(6, log2(num_regs)) when state = MASK else
+        to_unsigned(7, log2(num_regs)) when state = SET_BITS or state = UPDATE else
+        to_unsigned(3, log2(num_regs)) when state = ADD_A else to_unsigned(0, log2(num_regs));
 end Behavioral;
--------------- copy and pasted from lab 3 for reference -----------------
-
-
-
---architecture Behavioral of Control is
---type fsm_states is (IDLE, GEN_0, GEN_1, LD_R1, LD_R2, STORE, HOLD);
---signal state, next_state: fsm_states;
-----internal signals
---signal done, cnt_en : STD_LOGIC;
---signal cnt_out : UNSIGNED(4 downto 0);
---begin
-----combinational logic for the done signal
---done <= '1' when cnt_out =
-
----- defines each state transition
---state_transitions : process (state, nxt, done) is
---begin
---case state is
----- Reset state
---when IDLE =>
---if nxt = '1' then
---next_state <= GEN_0; -- Conditional transition (dependant on next)
---else
---next_state <= state; -- do nothing
---end if;
----- State 1
---when GEN_0 =>
---if nxt = '1' then
---next_state <= GEN_1;
---else
---next_state <= state;
---end if;
----- State 2
---when GEN_1 =>
---if nxt = '1'then
---next_state<=LD_R1;
---else
---next_state <= state;
---end if;
----- State 3
---when LD_R1 =>
---next_state<=LD_R2; -- unconditional transition (dependence on next removed)
----- State 4
---when LD_R2 =>
---next_state<=STORE;
----- State 5
---when STORE =>
---next_state<=HOLD;
----- State 6
---when HOLD => -- conditional transition
---if nxt = '1'then
---if (done = '0') then
---next_state <= LD_R1;
---elsif (done = '1') then
---next_state <= GEN_0;
---end if;
---else
---next_state <= state;
---end if;
---end case;
---end process state_transitions;
----- FSM outputs
----- declares output for high states then catches all with zero (except Mem_Addr)
---mem_wr <= '1' when state = GEN_0 or
---state = GEN_1 or
---state = STORE else
---'0'; -- catch all
---r1_en <= '1' when state = LD_R1 else
---'0';
---r2_en <= '1' when state = LD_R2 else
---'0';
---out_en <= '1' when state = GEN_0 or
---state = GEN_1 or
---state = STORE else
---'0';
---Mux_Sel <= "10" when state = GEN_0 else
---"11" when state = GEN_1 else
---"00";
---cnt_en <= nxt when state = GEN_0 or
---state = GEN_1 or
---state = HOLD else
---'0';
---Mem_Addr <= (cnt_out - 2) when state = LD_R1 else
---(cnt_out - 1) when state = LD_R2 else
---cnt_out; -- all other states are don't care
---end Behavioral;
-
---Counter VHDL
---library IEEE;
---use IEEE.STD_LOGIC_1164.ALL;
---use IEEE.NUMERIC_STD.ALL;
----- This entity describes a 5-bit Counter limited to 23 with enable and synchronous
----- reset.
--- entity CNT_to_24 is
---  Port ( clk : in STD_LOGIC;
---      en : in STD_LOGIC;
---      rst : in STD_LOGIC;
---      CNT_out : out UNSIGNED (4 downto 0));
--- end CNT_to_24;
--- architecture Behavioral of CNT_to_24 is
---  --internal signal for the counter
---  signal CNTInt: UNSIGNED (4 downto 0);
--- begin
-----Counter process
--- CNT: process (clk) -- No reset in sensitivity list
--- begin
---  if (rising_edge(clk)) then
---      if (rst = '1') then -- synchronous reset
---              CNTInt <= (others => '0');
---      else
---      if (en = '1') then
---              if(CNTInt = 24) then
---                  CNTInt <= (others => '0'); -- reset counter when it reaches 24
---              else
---                  CNTInt<=(CNTInt+1);
---              end if;
---      end if;
---      end if;
---  end if;
---end process CNT;
---  CNT_out <= CNTInt;
---end Behavioral;
